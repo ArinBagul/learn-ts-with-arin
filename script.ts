606a45283765d9599cf75eca488ec763b799f42a
Arin Bagul <prototypearin@gmail.com>
2024-07-26 15:42:22 +0530

    initial commit, implemented pages: home, install, extension, types ; also implemented some css and ts

---
+++ script.ts
@@ -0,0 +1,13 @@
+let a = [1,2,3,4];
+let b = a;
+
+b.pop();
+
+console.log(a);
+
+/* 
+    here at first a = [1,2,3,4]
+    we are popping out from b
+    now b = [1,2,3]
+    but also a = [1,2,3]
+*/
